970e0dc4915fadb9489a743d6113152eacc292f0
Lorenzo7991 <lorenzo1997chierisini@gmail.com>
2024-01-16 19:25:44 +0100

    add filter and map methods on students array

---
+++ JS/snack2.js
@@ -0,0 +1,16 @@
+console.log('S2-JS-OK');
+
+//*Filtering students with highscore over 70
+const highVoteStudents = studentsData.filter(student => student.grade > 70);
+
+console.log('Studenti con voto superiore a 70: ', highVoteStudents);
+
+//*Filtering students with highscore and cs id value
+const selectedCustomStudents = studentsData.filter(student => student.grade > 70 && student.id > 120);
+
+console.log('Studenti con voto superiore a 70 e id superiore a 120: ', selectedCustomStudents);
+
+
+//*Mapping list for names student with uppercase only
+const uppercaseNames = studentsData.map(student => ({ name: student.name.toUpperCase() }));
+console.log('Nomi degli studenti scritti in maiuscolo:', uppercaseNames);
