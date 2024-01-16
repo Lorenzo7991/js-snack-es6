cfcaa8c8774f2d18a79b2435310c888d912beef2
Lorenzo7991 <lorenzo1997chierisini@gmail.com>
2024-01-16 19:21:25 +0100

    add snacks data

---
+++ JS/data.js
@@ -0,0 +1,68 @@
+console.log('D-JS-OK');
+
+//^-----------------------------------------------------------------------------------------------------
+//^---------------------------------------------- SNACK 1 ----------------------------------------------
+//^-----------------------------------------------------------------------------------------------------
+
+const tableName = 'Tavolo Vip';
+
+const guests = [
+    'Brad Pitt',
+    'Johnny Depp',
+    'Lady Gaga',
+    'Cristiano Ronaldo',
+    'Georgina Rodriguez',
+    'Chiara Ferragni',
+    'Fedez',
+    'George Clooney',
+    'Amal Clooney',
+    'Maneskin'
+];
+
+console.log('Contenuto di partenza dell\' array dei VIP: ', guests);
+
+//^-----------------------------------------------------------------------------------------------------
+//^---------------------------------------------- SNACK 2 ----------------------------------------------
+//^-----------------------------------------------------------------------------------------------------
+
+
+
+const studentsData = [
+    {
+        id: 213,
+        name: 'Marco della Rovere',
+        grade: 78
+    },
+    {
+        id: 110,
+        name: 'Paola Cortellessa',
+        grade: 96
+    },
+    {
+        id: 250,
+        name: 'Andrea Mantegna',
+        grade: 48
+    },
+    {
+        id: 145,
+        name: 'Gaia Borromini',
+        grade: 74
+    },
+    {
+        id: 196,
+        name: 'Luigi Grimaldello',
+        grade: 68
+    },
+    {
+        id: 102,
+        name: 'Piero della Francesca',
+        grade: 50
+    },
+    {
+        id: 120,
+        name: 'Francesca da Polenta',
+        grade: 84
+    }
+];
+
+console.log('Contenuto di partenza dell\' array degli STUDENTI: ', studentsData);
