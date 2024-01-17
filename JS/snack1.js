d8b7edb8829d33ea6728145d059d9f0cb8467a2d
Lorenzo7991 <lorenzo1997chierisini@gmail.com>
2024-01-17 09:44:59 +0100

    fix snack1 syntax

--- JS/snack1.js
+++ JS/snack1.js
@@ -1,12 +1,7 @@
 console.log('S1-JS-OK');
 
-//*Creating array obj for guests
-const guestsList = guests.map((guest, index) => ({
-    tableName: tableName,
-    guestName: guest,
-    //*For occupied number
-    seatOccupied: index + 1,
-}));
+//* Creazione di un array di oggetti per gli ospiti
+const guestsList = guests.map((guest, i) => ({ tableName: tableName, guestName: guest, seatOccupied: i + 1 }));
 
-//*Console output
+//* Output sulla console
 console.log('Lista degli invitati VIP con segnaposto: ', guestsList);
