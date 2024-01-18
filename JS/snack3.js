12bf11bcdb920474c403de887c4f49321111e282
Lorenzo7991 <lorenzo1997chierisini@gmail.com>
2024-01-18 10:16:25 +0100

    add generic function for min Item

--- JS/snack3.js
+++ JS/snack3.js
@@ -1,6 +1,6 @@
 console.log('S3-JS-OK');
 
-//* Reducing to find min weighy obj
+/* //* Reducing to find min weighy obj
 const minWeight = bicycleArray.reduce((minBike, currentBike) => {
     return currentBike.weight < minBike.weight ? currentBike : minBike;
 });
@@ -9,3 +9,12 @@ const minWeight = bicycleArray.reduce((minBike, currentBike) => {
 const { brand, weight } = minWeight;
 //* Console output
 console.log(minWeight);
+ */
+
+const findMinimum = (array, key) => array.reduce((min, current) => current[key] < min[key] ? current : min, array[0]);
+
+const minWeightBike = findMinimum(bicycleArray, 'weight');
+
+const { brand, weight } = minWeightBike;
+
+console.log('Oggetto minore: ', minWeightBike);
